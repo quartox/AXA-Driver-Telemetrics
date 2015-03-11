8b798c7d88c3b51c96c7013809a6eabc80514eec
quartox <jessewlord@gmail.com>
2015-03-11 14:43:30 -0700

    The driver class which contains all trip x and y positions for each driver.

---
+++ driver.py
@@ -0,0 +1,10 @@
+"""The driver class."""
+
+__author__="Jesse Lord"
+__date__="March 10, 2015"
+
+class Driver:
+    def __init__(self,id):
+        self.id = id
+        self.x = list(list()) # all x-coordinates
+        self.y = list(list()) # all y-coordinates
