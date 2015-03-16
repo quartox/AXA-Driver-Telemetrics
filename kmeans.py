fe2b202dd20e38e6442491bc6a40986c299af11a
quartox <jessewlord@gmail.com>
2015-03-16 09:50:37 -0700

    Uses kmeans to identify different drivers. It is assumed that any large cluster represents the primary driver of the car.

--- kmeans.py
+++ kmeans.py
@@ -1,34 +1,18 @@
-"""Computes the regression analysis on the driver trip data."""
+"""Uses kmeans to compute which trips are the primary driver compared to any other drivers."""
 
 __author__="Jesse Lord"
 __date__="March 12, 2015"
 
-from features import computeFeatures
 from readData import readData
 import numpy as np
 import sklearn.cluster as cluster
 import sklearn.mixture as mix
 
-def combineFeatures(features,driverindex,nfeatures,nsamples):
-    x = np.empty((nsamples,nfeatures))
-    for ii in range(nsamples):
-        x[ii,0] = features[driverindex][ii].max_separation
-        x[ii,1] = features[driverindex][ii].final_distance
-        x[ii,2] = features[driverindex][ii].total_time
-        x[ii,3] = features[driverindex][ii].max_vel
-        x[ii,4] = features[driverindex][ii].mean_vel
-        x[ii,5] = features[driverindex][ii].max_accel
-        x[ii,6] = features[driverindex][ii].mean_accel
-        x[ii,7] = features[driverindex][ii].max_jerk
-        x[ii,8] = features[driverindex][ii].mean_jerk
-    return x
-
-
 def combineClusters(y,nclusters,nsamples):
     DEBUG = False
     num_positive = nsamples/nclusters
-    z1 = np.empty(0)
     z0 = np.empty(0)
+    z1 = np.empty(0)
     for ii in range(nclusters):
         (index,) = np.where(y == ii)
         if DEBUG:
@@ -37,20 +21,22 @@ def combineClusters(y,nclusters,nsamples):
             z1 = np.append(z1,index)
         else:
             z0 = np.append(z0,index)
+    z0 = z0.astype(int)
+    z1 = z1.astype(int)
     return (z0,z1)
 
-def kmeans():
-    drivers = readData()
-    features = computeFeatures(drivers)
-    nsamples = 200
-    nfeatures = features[0][0].nfeatures
 
-    x = combineFeatures(features,1,nfeatures,nsamples)
+def kmeans(x):
+
+    nsamples = 200
 
     nclusters = 5
 
-    km = cluster.KMeans(n_clusters=nclusters,n_init=100,init='k-means++')
+    km = cluster.KMeans(n_clusters=nclusters,n_init=20,init='k-means++')
     y = km.fit_predict(x)
 
     (z0,z1) = combineClusters(y,nclusters,nsamples)
-    return (z0,z1)
+    # randomly shuffling the indeces
+    np.random.shuffle(z0)
+    np.random.shuffle(z1)
+    return (x,z0,z1,nsamples)
